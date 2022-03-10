81f1e7dac03400db4025eadf0c5d19747666e9a6
root <ramonbritton@gmail.com>
2022-03-10 12:41:57 -0800

    updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,11 @@
 */
 int main(void)
 {
-	int i;
 	char c;
+	int i;
 	long int li;
 	long long ll;
 	float f;
-
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
        	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
