244365e5e8a63184a83447cf9cdfe060d6a15d7a
root <ramonbritton@gmail.com>
2022-03-14 01:53:47 -0700

    first commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Alyways 0 (Success)
+ */
+
+int main(void)
+{
+	int bets;
+	for(bets='z'; bets>='a'; bets--)
+	{
+		putchar(bets);
+	}
+	putchar('\n');
+	return(0);
+}
