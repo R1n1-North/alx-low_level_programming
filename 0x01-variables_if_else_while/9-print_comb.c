eb0d4368ed447669d81601dd970176595a1a3212
root <ramonbritton@gmail.com>
2022-03-14 04:29:41 -0700

    correcting 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int number;
-	for( number = 0; number < 10; number++ )
+	for (number = 0; number < 10; number++)
 	{
 		putchar(number + '0');
-		if( number < 9)
+		if (number < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar( '\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
