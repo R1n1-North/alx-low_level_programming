6fc4c8398de8f004fe7c8d2eeedb4d94b0df6923
root <ramonbritton@gmail.com>
2022-03-28 06:30:22 -0700

    even more arrays and pointers and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
+	while (*s != '\0')
 	{
-		if (s[a] == c)
-		return (&s[a]);
+		if (*s == c)
+		{
+			return (s);
+		}
+		else if (*(s + 1) == c)
+		{
+			return (s + 1);
+		}
+		s++;
 	}
-	return (0);
+	return (s + 1);
+	return (NULL);
 }
