3f16632ba45ca1d640f090ebed46d93f4fc22fc7
root <ramonbritton@gmail.com>
2022-04-15 03:20:48 -0700

    function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,1 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * get_op_func - has correct functon
+ * @s: operator passed as function
+ *
+ * Return: coreect function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
+		i++;
+	}
+
+	return (0);
+}
