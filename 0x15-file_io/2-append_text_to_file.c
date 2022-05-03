f1de7f433ff5aed7cc198d9d70450bb7137c3f45
root <ramonbritton@gmail.com>
2022-05-03 09:41:21 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,23 +7,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int reader, output, count;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	reader = open(filename, O_WRONLY | O_APPEND);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (reader == -1)
-		return (-1);
-	if (text_content == NULL)
-		return (1);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	for (; text_content[count] != '\0'; count++)
-		;
-	output = write(reader, text_content, count);
-	if (output == -1)
+	if (o == -1 || w == -1)
 		return (-1);
-	close(reader);
+	close(o);
+
 	return (1);
 }
